620ff57d9b9fe707ff10715d08985d59a1d46846
Michael Pfohl <rmpfohl@gmail.com>
2020-09-19 10:48:38 -0500

    add google authentication

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,16 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import authData from './helpers/data/authData';
+import auth from './components/auth';
+import nav from './components/navBar/navbar';
+
 import '../styles/main.scss';
 
 const init = () => {
-  console.warn('HELLO!');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  nav.logOutEvent();
 };
 
 init();
